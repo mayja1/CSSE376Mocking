e8ddf3431f1587f5e497b4635d422d3798111cb5
mayja1 <mayja1@rose-hulman.edu>
2015-04-04 21:45:20 -0400

    Added all additional test cases for the lab

--- ExpediaTest/CarTest.cs
+++ ExpediaTest/CarTest.cs
@@ -50,5 +50,38 @@ namespace ExpediaTest
 		{
 			new Car(-5);
 		}
+
+        [TestMethod]
+        public void TestGetLocationForCar()
+        {
+            IDatabase mockDB = mocks.StrictMock<IDatabase>();
+            String carLocation = "Terre Haute";
+            String anotherCarLocation = "Fort Wayne";
+            Expect.Call(mockDB.getCarLocation(24)).Return(carLocation);
+            Expect.Call(mockDB.getCarLocation(1025)).Return(anotherCarLocation);
+            mocks.ReplayAll();
+            Car target = new Car(10);
+            target.Database = mockDB;
+            String result;
+            result = target.getCarLocation(1025);
+            Assert.AreEqual(anotherCarLocation, result);
+            result = target.getCarLocation(24);
+            Assert.AreEqual(carLocation, result);
+            mocks.VerifyAll();
+         }
+
+        [TestMethod()]
+        public void TestThatCarDoesGetMileageFromDatabase()
+        {
+            IDatabase mockDatabase = mocks.StrictMock<IDatabase>();
+            Int32 Mileage = 10000;
+            Expect.Call(mockDatabase.Miles).PropertyBehavior();
+            mocks.ReplayAll();
+            mockDatabase.Miles = Mileage;
+            var target = ObjectMother.BMW();
+            target.Database = mockDatabase;
+            int roomCount = target.Mileage;
+            Assert.AreEqual(roomCount, Mileage);
+        }
 	}
 }
